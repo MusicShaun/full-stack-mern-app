e54aacbc9a1de90cc8711fa85f59a2f6c117901c
MusicShaun <music.shaunpickett@gmail.com>
2023-01-19 10:20:43 +0800

    jwt expiry must be a string

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -7,7 +7,7 @@ const AppError = require('../util/AppError')
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN
+    expiresIn: `${process.env.JWT_EXPIRES_IN}`
   })
 }
 
