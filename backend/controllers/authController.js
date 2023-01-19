f3d63b3179ba7dc78a83ff265a4d9233ff04721a
MusicShaun <music.shaunpickett@gmail.com>
2023-01-19 10:58:06 +0800

    Haven't found fix for heroku bug. Omitting jwt expiry

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,8 +6,10 @@ const AppError = require('../util/AppError')
 
 
 const signToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN * 24 * 60 * 60
+  return jwt.sign(
+    { id },
+    process.env.JWT_SECRET,
+    { 
   })
 }
 
