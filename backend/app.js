bdb5a172509c1ac075b0f39847083b7e8cbda3e5
MusicShaun <music.shaunpickett@gmail.com>
2023-01-19 13:48:39 +0800

    __dirname = path.resolve()

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ if (process.env.NODE_ENV === 'development') {
 
 
 // DEPLOYMENT 
-// __dirname = path.resolve()
+__dirname = path.resolve()
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, `/frontend/build`)))
